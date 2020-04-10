117e6f5bb3ee36271de9742d61e3f94044fa44c7
jmschonfeld <jeremyschonfeld@gmail.com>
2020-04-10 15:27:34 -0400

    Create bubble visualization

--- bubble.js
+++ bubble.js
@@ -1,55 +1,120 @@
-var diameterBubble = 960,
-    format = d3.format(",d"),
-    color = d3.scaleOrdinal(d3.schemeCategory10);
+// Setup the bubbles vizualization
+
+var vizSize = 200,
+    valueFormatter = d3.format(",d"),
+    scaleColor = d3.scaleOrdinal(d3.schemeCategory10);
 
 var bubble = d3.pack()
-    .size([diameterBubble, diameterBubble])
+    .size([vizSize, vizSize])
     .padding(1.5);
 
-var svgBubble = d3.select("#bubbles").append("svg")
-    .attr("width", diameterBubble)
-    .attr("height", diameterBubble)
+// Attach an svg element to the #bubbles element
+
+var svg = d3.select("#bubbles").append("svg")
+    .attr("width", vizSize)
+    .attr("height", vizSize)
     .attr("class", "bubble");
 
-d3.json("flare.json").then(function(data) {
+// Formats the collected JSON data into data that can be used for d3.heirarchy
+function formatData(data, key, date) {
+  if (!(key in data)) {
+    console.error("[BUBBLES] ERROR: attempted to show data for the topic '" + key + "' which is not a topic contained in the dataset.");
+    return;
+  }
+  var children = [];
+  for (const [subtopic, values] of Object.entries(data[key])) {
+    for (var j = 0; j < values.length; j++) {
+      if (values[j].date == date) {
+        children.push({topic: subtopic, value: values[j].val});
+        break;
+      }
+    }
+  }
+  if (children.length == 0) {
+    console.warn("[BUBBLES] WARNING: Unable to find any data for '" + key + "' on '" + date + "'. This date was not listed in the dataset.");
+  }
+  return {
+    name: "ROOT",
+    children: children
+  };
+}
 
-  var root = d3.hierarchy(classes(data))
+// Used to set the topic and date if the update function is called before the json is loaded
+defaultTopic = undefined;
+defaultDate = undefined;
+
+// Updates the vizualization with a new topic and date
+function updateBubbles(topic, date) {
+  if (!dataset) {
+    defaultTopic = topic;
+    defaultDate = date;
+  } else {
+    updateBubbles_active(topic, date);
+  }
+}
+
+function updateBubbles_active(topic, date) {
+  if (!dataset) {
+    console.error("[BUBBLES] ERROR: Called updateBubbles_active() before dataset has loaded. Please wait until D3 has loaded the dataset before calling updateBubbles_active().");
+    return;
+  }
+
+  // Create heirarchical bubble data
+  var root = d3.hierarchy(formatData(dataset, topic, date))
       .sum(function(d) { return d.value; })
       .sort(function(a, b) { return b.value - a.value; });
 
   bubble(root);
-  var node = svgBubble.selectAll(".node")
-      .data(root.children)
-    .enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
-  node.append("title")
-      .text(function(d) { return d.data.className + ": " + format(d.value); });
+  // Setup the circles
 
-  node.append("circle")
-      .attr("r", function(d) { return d.r; })
-      .style("fill", function(d) {
-        return color(d.data.packageName);
+  var nodes = svg.selectAll('.node').data(root.children);
+  var nodeEnter = nodes.enter()
+      .append("g")
+      .attr("class", "node");
+
+  console.log(nodes);
+
+  nodeEnter.append("title");
+  nodeEnter.append("circle");
+  nodeEnter.append("text")
+      .attr("dy", ".3em")
+      .style("text-anchor", "middle");
+
+  var update = nodes.merge(nodeEnter);
+
+  // Update existing circles
+  update.attr("transform", function(d) {
+        return "translate(" + d.x + "," + d.y + ")"; 
       });
 
-  node.append("text")
-      .attr("dy", ".3em")
-      .style("text-anchor", "middle")
-      .text(function(d) { return d.data.className.substring(0, d.r / 3); });
-});
+  update.select("text")
+      .text(function(d) {
+        if (d.data.topic.length >= d.r / 3) {
+          return d.data.topic.substring(0, d.r/3).trim() + "...";
+        } else {
+          return d.data.topic;
+        }
+      });
+  update.select("circle")
+      .attr("r", function(d) { return d.r; })
+      .style("fill", function(d) {
+        return scaleColor(d.data.topic);
+      });
+  update.select("title")
+      .text(function(d) { return d.data.topic + ": " + valueFormatter(d.value); });
 
-// Returns a flattened hierarchy containing all leaf nodes under the root.
-function classes(root) {
-  var classes = [];
-
-  function recurse(name, node) {
-    if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
-    else classes.push({packageName: name, className: node.name, value: node.size});
-  }
-
-  recurse(null, root);
-  return {children: classes};
+  // Remove old circles
+  nodes.exit().remove();
 }
 
-d3.select(self.frameElement).style("height", diameterBubble + "px");
+// Load the dataset
+d3.json("data_collection/data.json").then(function(data) {
+  dataset = data;
+
+  if (defaultTopic && defaultDate) {
+    // Call the update function if it was called before the data was loaded
+    updateBubbles_active(defaultTopic, defaultDate);
+  }
+  console.log("[BUBBLES] NOTICE: Data set loaded successfully");
+});
