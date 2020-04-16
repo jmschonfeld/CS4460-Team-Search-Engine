e318b43f2fcfd19bed1fb2f1d91d98f999198198
jmschonfeld <jeremyschonfeld@gmail.com>
2020-04-16 12:45:42 -0400

    Cleanup slider code and add callback

--- slider.js
+++ slider.js
@@ -2,63 +2,62 @@ var formatDateIntoYear = d3.timeFormat("%Y");
 var formatDate = d3.timeFormat("%m/%d/%Y");
 var parseDate = d3.timeParse("%m/%d/%y");
 
-var startDate;
-var endDate;
+var sliderMargin = {top:50, right:50, bottom:0, left:50},
+  sliderWidth = 960 - sliderMargin.left - sliderMargin.right,
+  sliderHeight = 500 - sliderMargin.top - sliderMargin.bottom;
 
-var margin = {top:50, right:50, bottom:0, left:50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-var svg = d3.select("#slider")
+var sliderSVG = d3.select("#slider")
     .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
+    .attr("width", sliderWidth + sliderMargin.left + sliderMargin.right)
+    .attr("height", sliderHeight + sliderMargin.top + sliderMargin.bottom);
 
+// Slider Callback Function
+// Call setSliderCallback with a function. This function will be called every time the slider selects a new date
+var sliderCallback = undefined;
+function setSliderCallback(cbFunction) {
+  sliderCallback = cbFunction;
+}
 
 d3.json("/data_collection/play_around.json").then(function(data) {
 
+
   var dateDict = data['obesity']['childhood obesity'];
-  startDate = new Date(dateDict['start_date']);
-  endDate = new Date(dateDict['end_date']);
-
-  var textValue = dateDict[dateDict['start_date']];
-
-  var moving = false;
-  var currentValue = 0;
-  var currentIndex = 0;
-  var targetValue = width;
+  var startDate = new Date(dateDict['start_date']);
+  var endDate = new Date(dateDict['end_date']);
 
   var playButton = d3.select("#play-button");
 
   var x = d3.scaleTime()
       .domain([startDate, endDate])
-      .range([0, targetValue])
+      .range([0, sliderWidth])
       .clamp(true);
 
-  var slider = svg.append("g")
+  var slider = sliderSVG.append("g")
       .attr("class", "slider")
-      .attr("transform", "translate(" + margin.left + "," + height/5 + ")");
+      .attr("transform", "translate(" + sliderMargin.left + "," + sliderHeight/5 + ")");
 
+  // Slider line
   slider.append("line")
       .attr("class", "track")
       .attr("x1", x.range()[0])
       .attr("x2", x.range()[1])
-    .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+      .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
       .attr("class", "track-inset")
-    .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+      .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
       .attr("class", "track-overlay")
       .call(d3.drag()
-          .on("start.interrupt", function() { slider.interrupt(); })
-          .on("start drag", function() {
-            currentValue = d3.event.x;
-            update(x.invert(currentValue));
-          })
+        .on("start.interrupt", function() { slider.interrupt(); })
+        .on("start drag", function() {
+          var currentValue = d3.event.x;
+          update(x.invert(currentValue));
+        })
       );
 
+  // Slider ticks
   slider.insert("g", ".track-overlay")
       .attr("class", "ticks")
       .attr("transform", "translate(0," + 18 + ")")
-    .selectAll("text")
+      .selectAll("text")
       .data(x.ticks(10))
       .enter()
       .append("text")
@@ -68,36 +67,33 @@ d3.json("/data_collection/play_around.json").then(function(data) {
       .attr("text-anchor", "middle")
       .text(function(d) { return formatDateIntoYear(d); });
 
+  // Slider handle
   var handle = slider.insert("circle", ".track-overlay")
       .attr("class", "handle")
       .attr("r", 9);
 
+  // Date label
   var label = slider.append("text")
       .attr("class", "label")
       .attr("text-anchor", "middle")
       .text(formatDate(startDate))
       .attr("transform", "translate(0," + (-25) + ")")
-      .attr("font-size", "10px")
+      .attr("font-size", "10px");
 
+  function update(dateObj) {
+    // Format date object into MM/DD/YYYY format
+    var date = formatDate(dateObj);
 
-  //adding text element to svg
-  var text = svg.append("text")
-                .text(textValue)
-                .attr("x", 150)
-                .attr("y", 150)
-                .attr("font-size", "35px")
-
-  function update(h, i) {
     // update position and text of label according to slider scale
-    handle.attr("cx", x(h));
-    label
-      .attr("x", x(h))
-      .text(formatDate(h));
-    //Displaying text to page
+    handle.attr("cx", x(dateObj));
+    label.attr("x", x(dateObj))
+      .text(date);
 
-    text.text(dateDict[formatDate(h)]);
+    // Call the callback function (if set)
+    if (sliderCallback) {
+      sliderCallback(date);
+    }
   }
-
 });
 
 
